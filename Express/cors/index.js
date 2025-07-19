deda9b7186cac87b01371e84592d415b71911790
Sakibul Alam <sakibulalam557@gmail.com>
2025-07-19 13:34:26 +0600

    Update files

---
+++ Express/cors/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/sum", function (req, res) {
+  const a = parseInt(req.query.a);
+  const b = parseInt(req.query.b);
+
+  res.json({
+    ans: a + b,
+  });
+});
+
+app.listen(3000);
